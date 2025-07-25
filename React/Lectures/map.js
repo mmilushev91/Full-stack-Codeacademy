b8ec0a6622bf30fe81636fab49b1c018e5e83547
mmilushev91 <milushev_1990@abv.bg>
2025-07-25 15:50:17 +0300

    Create map.js

---
+++ React/Lectures/map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleList = people.map((person,i) =>
+  // expression goes here:
+  <li key={"person_" + i}>{person}</li>;
+);
+
+// root.render goes here:
+root.render(<ul>{peopleList}</ul>);
